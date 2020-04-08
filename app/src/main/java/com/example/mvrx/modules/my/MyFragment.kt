a3dd0ed2d78689da7d48ea22f6e92dda2314867b
Ruangguru <ruangguru@Ruanggurus-MacBook-Pro.local>
2020-04-08 14:09:50 +0700

    subscribe state manually

--- app/src/main/java/com/example/mvrx/modules/my/MyFragment.kt
+++ app/src/main/java/com/example/mvrx/modules/my/MyFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mvrx.modules.my
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -34,6 +35,17 @@ class MyFragment : BaseMvRxFragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+
+        viewModel.subscribe { state ->
+            Log.d("MyFragmentState", "State = $state")
+
+            when(state.temperature) {
+                is Uninitialized -> temperature.text = "N/A"
+                is Loading -> temperature.text = "Loading"
+                is Success -> temperature.text = "${state.temperature()} degrees"
+                is Fail -> temperature.text = "Failed"
+            }
         }
     }
 
